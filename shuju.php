085ca51a6fb2772d836b93bdcffa80d3589b64f5
王亚索 <1047429866@qq.com>
2018-11-08 17:13:07 +0800

    Add files via upload

---
+++ shuju.php
@@ -0,0 +1,16 @@
+<?php
+/*$arr=array(
+   'uid'=>1,
+   'classid'=>2,
+   'data'=>array(
+      'grade'=>1,
+	  'income'=>100
+   )
+);
+echo json_encode($arr);exit;*/
+$arr=array(
+   'name'=>'xiaoming'
+);
+
+
+
